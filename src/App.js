b73fb98c7a9afe8a4d516269ca91862a0ab008cb
Ramana-vummenthala <57570449+Ramana-vummenthala@users.noreply.github.com>
2025-01-21 15:40:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React From React Tutorials
+          Learn React 
         </a>
       </header>
     </div>
